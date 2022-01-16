71629b36d6ada3ee8f2d55b18b5a66070f1a5ff3
sovni <christophe.burgalat@gmail.com>
2022-01-16 10:40:33 +0100

    Fix warnings and install hdwallet

--- contracts/MyDefiProject.sol
+++ contracts/MyDefiProject.sol
@@ -1,10 +1,11 @@
-// MyDeFiProject.sol
-pragma solidity 0.6.12;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
  
 contract MyDeFiProject {
- 
- constructor() public {
+ IERC20 dai;
+
+ constructor() {
    // injecter l'address du token Dai à utiliser
    dai = IERC20(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
  }
